310bef48cd58120ecb02e01a3ea2318330590c2e
Jafar Aziz <jhaziz747@gmail.com>
2021-07-24 02:38:01 +0700

    remove unused component

--- components/layout.js
+++ components/layout.js
@@ -1,18 +1,14 @@
-import Alert from './alert'
-import Footer from './footer'
 import Meta from './meta'
 import 'lazysizes'
 import 'lazysizes/plugins/parent-fit/ls.parent-fit'
 
-export default function Layout({ preview, children }) {
+export default function Layout({ children }) {
   return (
     <>
       <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
+      <div className="m-auto max-w-screen-lg min-h-screen">
         <main>{children}</main>
       </div>
-      <Footer />
     </>
   )
 }
