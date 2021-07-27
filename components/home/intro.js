7813ac4200afcff406a5220fcbbd6aa9a258b2e9
Jafar Aziz <jhaziz747@gmail.com>
2021-07-27 17:06:28 +0700

    test lighthouse

--- components/home/intro.js
+++ components/home/intro.js
@@ -4,9 +4,9 @@ export default function Intro({title, description}) {
       <h1 className="text-5xl font-bold tracking-tighter leading-tight md:pr-8">
         {title}
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+      <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
         {description}
-      </h4>
+      </h2>
     </section>
   )
 }
--- components/layouts/header.js
+++ components/layouts/header.js
@@ -4,7 +4,7 @@ const Header = ({ settings }) => {
     <header className="border-b-2">
       <div className="max-w-screen-lg mx-auto px-5 py-5">
         <a href="/">
-          <img src={logo} width={36} heigh={36} />
+          <img src={logo} alt="website-icon" aria-label="website-icon" width={36} heigh={36} />
         </a>
       </div>
     </header>
